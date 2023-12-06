aa7878ef6bad16760b95599c78397b381b080909
Anupama Bansal <81766757+abansal-redesign@users.noreply.github.com>
2023-12-06 22:44:38 +0530

    Update aoc_05.py

--- aoc_05.py
+++ aoc_05.py
@@ -11,166 +11,153 @@ class SeedData:
         self.loc = loc
 
 class SeedSoilMap:
-    def __init__(self, seed_soil_data):
-        self.data = seed_soil_data
-        self.map = {}
-        for each in self.data:
-            self.find_map(each)
-        
-    def find_map(self, line_data):
+    def __init__(self, data):
+        self.data = data
+                
+    def find_map_val(self, src_val):
+        val = None
         for each_line_data in self.data:
-            des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
-            for i in range(0, length):
-                self.map[src + i] = des + i
+            if not val:
+                des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
+                if src_val in range(src, src + length):
+                    val = des + (src_val - src)
+        if not val:
+            val = src_val
+        return val
 
 class SoilFertilizerMap:
     def __init__(self, data):
         self.data = data
-        self.map = {}
-        for each in self.data:
-            self.find_map(each)
-        
-    def find_map(self, line_data):
+
+    def find_map_val(self, src_val):
+        val = None
         for each_line_data in self.data:
-            des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
-            for i in range(0, length):
-                self.map[src + i] = des + i
+            if not val:
+                des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
+                if src_val in range(src, src + length):
+                    val = des + (src_val - src)
+        if not val:
+            val = src_val
+        return val
 
 class FertilizerWaterMap:
     def __init__(self, data):
         self.data = data
-        self.map = {}
-        for each in self.data:
-            self.find_map(each)
-        
-    def find_map(self, line_data):
+
+    def find_map_val(self, src_val):
+        val = None
         for each_line_data in self.data:
-            des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
-            for i in range(0, length):
-                self.map[src + i] = des + i
+            if not val:
+                des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
+                if src_val in range(src, src + length):
+                    val = des + (src_val - src)
+        if not val:
+            val = src_val
+        return val
 
 class WaterLightMap:
     def __init__(self, data):
         self.data = data
-        self.map = {}
-        for each in self.data:
-            self.find_map(each)
-        
-    def find_map(self, line_data):
+
+    def find_map_val(self, src_val):
+        val = None
         for each_line_data in self.data:
-            des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
-            for i in range(0, length):
-                self.map[src + i] = des + i
+            if not val:
+                des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
+                if src_val in range(src, src + length):
+                    val = des + (src_val - src)
+        if not val:
+            val = src_val
+        return val
 
 class LightTempMap:
     def __init__(self, data):
         self.data = data
-        self.map = {}
-        for each in self.data:
-            self.find_map(each)
-        
-    def find_map(self, line_data):
+
+    def find_map_val(self, src_val):
+        val = None
         for each_line_data in self.data:
-            des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
-            for i in range(0, length):
-                self.map[src + i] = des + i
+            if not val:
+                des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
+                if src_val in range(src, src + length):
+                    val = des + (src_val - src)
+        if not val:
+            val = src_val
+        return val
 
 class TempHumidMap:
     def __init__(self, data):
         self.data = data
-        self.map = {}
-        for each in self.data:
-            self.find_map(each)
-        
-    def find_map(self, line_data):
+
+    def find_map_val(self, src_val):
+        val = None
         for each_line_data in self.data:
-            des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
-            for i in range(0, length):
-                self.map[src + i] = des + i
+            if not val:
+                des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
+                if src_val in range(src, src + length):
+                    val = des + (src_val - src)
+        if not val:
+            val = src_val
+        return val
 
 class HumidLocationMap:
     def __init__(self, data):
         self.data = data
-        self.map = {}
-        for each in self.data:
-            self.find_map(each)
-        
-    def find_map(self, line_data):
+
+    def find_map_val(self, src_val):
+        val = None
         for each_line_data in self.data:
-            des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
-            for i in range(0, length):
-                self.map[src + i] = des + i
+            if not val:
+                des, src, length = each_line_data[0], each_line_data[1], each_line_data[2]
+                if src_val in range(src, src + length):
+                    val = des + (src_val - src)
+        if not val:
+            val = src_val
+        return val
+            
 
 class AnalyseData:
     def __init__(self, all_data):
         self.all_data = all_data
-        self.seed_inst = []
         self.map_all_data()
-        self.find_seed_data()
+        self.least_loc_val = None
         
     def map_all_data(self):
-        self.seed_to_soil = SeedSoilMap(self.all_data['seed-to-soil']).map
-        self.soil_to_fert = SoilFertilizerMap(self.all_data['soil-to-fertilizer']).map
-        self.fert_to_water = FertilizerWaterMap(self.all_data['fertilizer-to-water']).map
-        self.water_to_light = WaterLightMap(self.all_data['water-to-light']).map
-        self.light_to_temp = LightTempMap(self.all_data['light-to-temperature']).map
-        self.temp_to_humid = TempHumidMap(self.all_data['temperature-to-humidity']).map
-        self.humid_to_loc = HumidLocationMap(self.all_data['humidity-to-location']).map
-        self.seeds = self.all_data['seeds'][0]
+        self.seed_to_soil = SeedSoilMap(self.all_data['seed-to-soil'])
+        self.soil_to_fert = SoilFertilizerMap(self.all_data['soil-to-fertilizer'])
+        self.fert_to_water = FertilizerWaterMap(self.all_data['fertilizer-to-water'])
+        self.water_to_light = WaterLightMap(self.all_data['water-to-light'])
+        self.light_to_temp = LightTempMap(self.all_data['light-to-temperature'])
+        self.temp_to_humid = TempHumidMap(self.all_data['temperature-to-humidity'])
+        self.humid_to_loc = HumidLocationMap(self.all_data['humidity-to-location'])
     
-    def find_seed_data(self):
-        for each in self.seeds:
-            seed = each
-            print(seed)
-            # find soil from seed values
-            if seed in self.seed_to_soil:
-                soil = self.seed_to_soil[seed]
-            else:
-                soil = seed
-            # find fert from soil values
-            if soil in self.soil_to_fert:
-                fert = self.soil_to_fert[soil]
-            else:
-                fert = soil
-            # find water from fert values
-            if fert in self.fert_to_water:
-                water = self.fert_to_water[fert]
-            else:
-                water = fert
-            # find light from water values
-            if water in self.water_to_light:
-                light = self.water_to_light[water]
-            else:
-                light = water
-            # find temp from light values
-            if light in self.light_to_temp:
-                temp = self.water_to_light[light]
-            else:
-                temp = light
-            # find humid from temp values
-            if temp in self.temp_to_humid:
-                humid = self.temp_to_humid[temp]
-            else:
-                humid = temp
-            # find location from humid values
-            if humid in self.humid_to_loc:
-                loc = self.humid_to_loc[humid]
-            else:
-                loc = humid
-            seed_inst = SeedData(seed, soil, fert, water, light, temp, humid, loc)
-            self.seed_inst.append(seed_inst)
-            
-    def least_location_value(self):
-        location_val = None
-        for each in self.seed_inst:
-            if not location_val:
-                location_val = each.loc
-            if each.loc < location_val:
-                location_val = each.loc
-        return location_val
+    def find_seed_data(self, seed):
+        # find soil from seed values
+        soil = self.seed_to_soil.find_map_val(seed)
+        # find fert from soil values
+        fert = self.soil_to_fert.find_map_val(soil)
+        # find water from fert values
+        water = self.fert_to_water.find_map_val(fert)
+        # find light from water values
+        light = self.water_to_light.find_map_val(water)
+        # find temp from light values
+        temp = self.light_to_temp.find_map_val(light)
+        # find humid from temp values
+        humid = self.temp_to_humid.find_map_val(temp)
+        # find location from humid values
+        loc = self.humid_to_loc.find_map_val(humid)
+        # print(seed, soil, fert, water, light, temp, humid, loc)
+        seed_inst = SeedData(seed, soil, fert, water, light, temp, humid, loc)
+        self.update_least_location_value(seed_inst)
+
+    def update_least_location_value(self, seed_inst):
+        if not self.least_loc_val:
+            self.least_loc_val = seed_inst.loc
+        if seed_inst.loc < self.least_loc_val:
+            self.least_loc_val = seed_inst.loc
 
 def main():
-    data = open('aoc_05_sample.txt', 'r').readlines()
+    data = open('aoc_05.txt', 'r').readlines()
     all_data = {}
     heading = None
     for each in data:
@@ -184,7 +171,11 @@ def main():
             all_data[heading].append(converted_data)
 
     analyse_data = AnalyseData(all_data)
-    print(analyse_data.least_location_value())
+    seeds = all_data['seeds'][0]
+    for i in range(0, len(seeds), 2):
+        for j in range(0, seeds[i + 1]):
+            analyse_data.find_seed_data(seeds[i] + j)
+    print(analyse_data.least_loc_val)
     
 main()
     
